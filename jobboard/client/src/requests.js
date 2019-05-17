417b3da127123b93356d624d08f639b30f2837a0
Daniel Carlson <dancarl758@gmail.com>
2019-05-17 17:28:46 +0100

    Completed jobboard application

---
+++ jobboard/client/src/requests.js
@@ -0,0 +1,83 @@
+import { getAccessToken, isLoggedIn } from './auth';
+
+const endpointURL = 'http://localhost:9000/graphql';
+
+async function graphqlRequest(query, variables={}) {
+  const request = {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify({query, variables})
+  };
+  if (isLoggedIn()) {
+    request.headers['authorization'] = 'Bearer ' + getAccessToken();
+  }
+  const response = await fetch(endpointURL, request);
+  const responseBody = await response.json();
+  if (responseBody.errors) {
+    const message = responseBody.errors.map((error) => error.message).join('\n');
+    throw new Error(message);
+  }
+  return responseBody.data;
+}
+
+export async function createJob(input) {
+  const mutation = `mutation CreateJob($input: CreateJobInput) {
+    job: createJob(input: $input) {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`;
+  const {job} = await graphqlRequest(mutation, {input});
+  return job;
+}
+
+export async function loadCompany(id) {
+  const query = `query CompanyQuery($id: ID!) {
+    company(id: $id) {
+      id
+      name
+      description
+      jobs {
+        id
+        title
+      }
+    }
+  }`;
+  const {company} = await graphqlRequest(query, {id});
+  return company;
+}
+
+export async function loadJob(id) {
+  const query = `query JobQuery($id: ID!) {
+    job(id: $id) {
+      id
+      title
+      company {
+        id
+        name
+      }
+      description
+    }
+  }`;
+  const {job} = await graphqlRequest(query, {id});
+  return job;
+}
+
+export async function loadJobs() {
+  const query = `{
+    jobs {
+      id
+      title
+      company {
+        id
+        name
+      }
+    }
+  }`;
+  const {jobs} = await graphqlRequest(query);
+  return jobs;
+}
